2cb408c9531906875fa9f0f9f60b4a1b1b502267
J.D. Whirley <86266189+jdwhirley@users.noreply.github.com>
2022-06-07 12:40:10 -0700

    How many users are there?

--- querycliquebait.sql
+++ querycliquebait.sql
@@ -1,6 +1,24 @@
+--Digital analysis
+--How many users are there?
+SELECT COUNT ( DISTINCT user_id ) AS "Number of users" 
+FROM clique_bait.users;
+
+--How many cookies does each user have on average?
+
+
+
+--Product funnel analysis
 --How many times was each product viewed?
 SELECT page_id, event_type, COUNT(*)
 FROM clique_bait.events 
 WHERE event_type=1
 GROUP BY page_id, events.event_type;
 
+--How many times was each product added to cart?
+SELECT page_id, event_type, COUNT(*)
+FROM clique_bait.events 
+WHERE event_type=2
+GROUP BY page_id, events.event_type;
+
+--How many times was each product purchased?
+;
